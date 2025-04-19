bc1b0c722f0d5eedbc050eee141a1751e6c7facd
linoy <linoy@gmail.com>
2025-04-19 10:36:00 +0300

    create validations and tools

---
+++ src/formValidation.ts
@@ -0,0 +1,25 @@
+const validation = {
+    username:{
+        required  : "שדה זה חובה",
+        minLength : {value:4  , message:"must between 4 - 10"},
+        maxLength : {value:10 , message:"must between 4 - 10"},
+    },
+    password:{
+        required  : "שדה זה חובה",
+        minLength : {value:6  , message:"must between 6 - 20"},
+        maxLength : {value:20 , message:"must between 6 - 20"},
+    },
+    email:{
+        required : "שדה זה חובה",
+    },
+    title: {
+        required:"שדה זה חובה",
+        minLength:{value:4  , message:"must between 4 - 50"},
+        maxLength:{value:50 , message:"must between 4 - 50"},
+    },
+    context: {
+        required:"שדה זה חובה",
+    }
+}
+
+export {validation};
---
+++ src/tools.ts
@@ -0,0 +1,42 @@
+import IUserPayload from "./interface/IUserPayload";
+import { IMethod } from "./interface/types";
+
+const serverUrl = "http://localhost:3002";
+
+function Fetch<T=any>(api:string, method:IMethod, body={}):Promise<T>{
+    const token = localStorage.getItem("token");
+    let obj :any = {method,headers:{
+        "Content-Type":"application/json",
+        "Authorization": "Bearer " + token
+    }}
+    if(method!="GET"){obj["body"]= JSON.stringify(body)}
+    return new Promise((resolve,reject)=>{
+        fetch(serverUrl+api,obj)
+        .then(d=>d.json())
+        .then(data=> {
+            console.log("Fetch-data:",data);
+            resolve(data)}
+        )
+        .catch(err=> reject(err))
+    })
+}
+
+function isLoggedIn():boolean{
+    if(!localStorage.getItem("token")) 
+        return false;
+    else
+        return true;
+}
+
+function getUserPayload():IUserPayload{
+    let payload =  localStorage.getItem("payload");
+    if(payload)
+        return JSON.parse(payload) as IUserPayload;
+    else
+        return {} as IUserPayload;
+}
+
+
+export {Fetch}
+export {isLoggedIn}
+export {getUserPayload}
